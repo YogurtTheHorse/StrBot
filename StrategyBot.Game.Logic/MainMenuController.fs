1505d97c072f095909913fd223563419bd641fd6
YogurtTheHorse <yegorf1@gmail.com>
2020-02-07 19:26:14 +0300

    F# is power!

---
+++ StrategyBot.Game.Logic/MainMenuController.fs
@@ -0,0 +1,20 @@
+namespace StrategyBot.Game.Logic
+
+open StrategyBot.Game.Core
+open StrategyBot.Game.Core.Controllers
+open StrategyBot.Game.Core.Controllers.Abstractions
+open StrategyBot.Game.Core.Controllers.Handlers
+
+[<Controller(isMainController = true)>]
+type MainMenuController() =
+    inherit ControllerBase()
+
+    [<Action("screens.main_menu.say_my_name")>]
+    member __.SayMyName(player: PlayerState ) =
+        __.Answer ("Your name is " + player.SocialId)
+
+    [<Action("screens.main_menu.attack")>]
+    member __.AttackAction() = __.Answer "test"
+
+    [<Default>]
+    member __.DefaultAction() = __.Answer "default"
